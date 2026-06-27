6a3d6509f151b4df4272dd21a205b6cc6538ea3c
Adrian Guerra <adrian.guerra@cuh.mx>
2026-06-27 09:12:42 -0600

    resolucion de errores de nuevo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useServerFn } from "@tanstack/react-start";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import {
   getDashboardData,
   scanCheckIn,
